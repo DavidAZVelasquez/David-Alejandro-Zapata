a278b5c26892817c72b77ac55b84748a8fcb3847
DavidAZVelasquez <124403192+DavidAZVelasquez@users.noreply.github.com>
2023-02-17 17:22:02 -0500

    Trabajo en Clase 02/17/23
    David Alejandro Zapata Velásquez

---
+++ dom.js
@@ -0,0 +1,25 @@
+console.log("Entramos");
+
+var items= document.getElementsByClassName("Item");
+
+var cantidad= items.length; 
+
+console.log(cantidad);
+console.log("Cantidad de listas " + cantidad);
+var div= document.createElement("Div");
+
+div;
+
+div.innerText = "Aprendiendo JavaScript";
+var divUno = document.getElementById("uno");
+
+var Lista = document.getElementById("lista");
+var hijo = document.createElement("li");
+hijo.innerText="Li nuevo";
+Lista.appendChild(hijo);
+
+var par=document.getElementById('tres').style.color='#FF0033';
+var par=document.getElementById('lista').style.color='#00FF33';
+
+var parra2=document.getElementById("parra2").style.backgroundcolor="Blue";           
+var par2=document.createElement("p");
